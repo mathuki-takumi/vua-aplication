4f614354de855dda9c7cd542171f9460f0c015ce
mathuki-takumi <124752059+mathuki-takumi@users.noreply.github.com>
2023-10-24 10:53:03 +0900

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,71 +1,137 @@
+<meta charset="UTF-8">
+
 <template>
-  <div class="hello">
-    <h1>{{ localMsg }}</h1>
-    <input v-model="localMsg" />
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div id="profile_container">
+
+      <img id="profile_img" src="../assets/avatar.png" alt="" />
+    <h2>松木拓海</h2>
+    <div id="profile_description">
+    <p class="profile_text">
+        {{ Birthdate.year }}年{{ Birthdate.month }}月{{ Birthdate.day }}日生まれ
+  {{ Age }}歳
+      </p>
+      
+      <div id="profile_intro" class="profile_text">
+        <p>
+          
+        </p>
+      </div>
+    </div>
+  
   </div>
 </template>
 
-
-
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
   data() {
     return {
-      localMsg: this.msg
+      Age: 21,
+      Birthdate: {
+        day: 13,
+        month: 10,
+        year: 2002,
+      },
+    
     };
   },
-  watch: {
-    msg(newVal) {
-      this.localMsg = newVal;
-    }
-  }
-}
+created() {
+    this.InitAge();
+    this.InitPosition();
+  },
+ methods: {
+    InitAge() {
+      let birthdate = new Date(
+        this.Birthdate.year,
+        this.Birthdate.month - 1,
+        this.Birthdate.day
+      );
+      let today = new Date();
+      let age = today.getFullYear() - birthdate.getFullYear();
+      if (
+        today.getMonth() < birthdate.getMonth() ||
+        (today.getMonth() === birthdate.getMonth() &&
+        today.getDate() < birthdate.getDate())
+      ) {
+        age--;
+      }
+      this.Age = age;
+    },
+    InitPosition() {
+      let grade = this.Age - 21;
+      if (grade < 1) {
+        this.Position = "HAL大阪ゲーム制作学科" + (4 - grade) + "年生";
+      } else {
+        this.Position = "�Љ�l" + grade + "�N��";
+      }
+    },
+ },
+  
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+<style scoped>lang="scss" scoped>
+  #profile_container {
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    #profile_img {
+      width: 300px;
+      height: 300px;
+      border-radius: 100px;
+      margin: 15px;
+    }
+    @media screen and (max-width: 400px) {
+      #profile_img {
+        width: 200px;
+        height: 200px;
+        border-radius: 75px;
+      }
+    }
+    #profile_description {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      .profile_text {
+        padding: 10px;
+        font-size: 19px;
+      }
+      @media screen and (max-width: 400px) {
+        .profile_text {
+          padding: 10px 25px;
+        }
+      }
+    }
+    #socials {
+      width: 100%;
+      height: auto;
+      margin-bottom: 20px;
+      display: flex;
+      flex-wrap: wrap;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      .social_icon {
+        width: 100px;
+        height: 100px;
+        border-radius: 100px;
+        margin: 10px;
+        img {
+          width: 100%;
+          height: 100%;
+          border-radius: 100px;
+          transition: 0.3s;
+          &:hover {
+            box-shadow: 0px 2px 10px -2px #777777;
+          }
+        }
+      }
+    }
+  }
 </style>
+
