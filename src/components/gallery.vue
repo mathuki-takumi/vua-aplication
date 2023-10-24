43c0be24b3d945e4e8bab049bb800d08b5251bf0
mathuki-takumi <124752059+mathuki-takumi@users.noreply.github.com>
2023-10-24 13:47:40 +0900

    Create gallery.vue

---
+++ src/components/gallery.vue
@@ -0,0 +1,26 @@
+<meta charset="UTF-8">
+
+<template>
+
+<div id="gallery_container">
+    <input
+      id="gallery_searchbar"
+      type="text"
+      placeholder="ƒMƒƒƒ‰ƒŠ[‚ðŒŸõ"
+      @input="UpdateCards"
+      v-model="search_text"
+    />
+    <div id="galleries">
+      <div class="card" v-for="card in disp_cards" :key="card.id">
+        <img class="card_img" :src="card.image" />
+        <div class="card_title">
+          {{ card.title }}
+        </div>
+        <div class="card_description">
+          {{ card.description }}
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </template>
